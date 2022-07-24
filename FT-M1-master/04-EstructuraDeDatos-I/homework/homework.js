53e121affe76427447a07b20d39ad2252bec6c03
Ramiro-Ruggeri <ramirorruggeri@gmail.com>
2022-07-23 23:12:03 -0300

    first commit

--- FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
@@ -15,9 +15,17 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if(n===0) return 0;
+  if(n===1) return 1;
+  return (n) * nFactorial(n-1);
 }
 
 function nFibonacci(n) {
+  if(n===0) return 0;
+  if(n===1) return 1;
+  if(n>1) {
+    return nFibonacci(n-1) + nFibonacci(n-2);
+  }
 }
 
 /*
@@ -28,12 +36,26 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 
 Pueden utilizar class o función constructora.
 */
-
+var array = [];
 function Queue() {
-
+  
+}
+Queue.prototype.enqueue = function(val){
+  this.array.push(val)
+}
+Queue.prototype.dequeue = function(){
+  return this.shift();
+}
+Queue.prototype.size = function(){
+  return this.length
 }
 
+
+
 // No modifiquen nada debajo de esta linea
+
+
+
 // --------------------------------
 
 module.exports = {
